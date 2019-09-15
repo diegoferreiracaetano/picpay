5b55fbbe783c1fdb93348cae7fc8ee9eec6ea06c
diego ferreira caetano <di.cafe@hotmail.com>
2019-09-15 19:25:17 -0300

    configuration sonar

---
+++ domain/src/test/kotlin/com/diegoferreiracaetano/domain/user/UserTest.kt
@@ -0,0 +1,49 @@
+package com.diegoferreiracaetano.domain.user
+
+import com.diegoferreiracaetano.domain.Mock
+import io.mockk.coEvery
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+
+class UserTest {
+
+    lateinit var user: User
+
+    @Before
+    fun setUp() {
+        user = User(id = 0, name = "name", username = "username", img = null)
+    }
+
+    @Test
+    fun `when call id should value zero`() {
+
+        assertEquals(0, user.id)
+    }
+
+    @Test
+    fun `when call name should value name`() {
+
+        assertEquals("name", user.name)
+    }
+
+    @Test
+    fun `when call username should value username`() {
+
+        assertEquals("username", user.username)
+    }
+
+    @Test
+    fun `when call img should value img null`() {
+
+        assertNull(user.img)
+    }
+
+    @Test
+    fun `when call img should value img not null`() {
+        user = User(id = 0, name = "name", username = "username", img = "http://www.google.com/img.jpg")
+
+        assertNotNull(user.img)
+    }
+}
