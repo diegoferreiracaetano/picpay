659138b5ce814fd92d4cc332dc5ea59b62e79b8e
diego ferreira caetano <di.cafe@hotmail.com>
2019-09-22 07:57:12 -0300

    ajuste teste mainactivity

--- app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/ui/MainActivityTest.kt
+++ app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/ui/MainActivityTest.kt
@@ -1,22 +1,17 @@
-package com.diegoferreiracaetano.picpay
+package com.diegoferreiracaetano.picpay.ui
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class MainActivityTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = ApplicationProvider.getApplicationContext<Context>()
         assertEquals("com.diegoferreiracaetano.picpay", appContext.packageName)
     }
