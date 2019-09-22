e4032c107b225119e209ce5fcdc19b522011deaf
diego ferreira caetano <di.cafe@hotmail.com>
2019-09-22 11:27:27 -0300

    ajuste teste mainactivity

--- app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/ui/MainActivityTest.kt
+++ app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/ui/MainActivityTest.kt
@@ -1,5 +1,6 @@
 package com.diegoferreiracaetano.picpay.ui
 
+import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
@@ -11,18 +12,19 @@ import com.diegoferreiracaetano.picpay.R
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-class MyActivityTest {
 
-    private lateinit var scenario: ActivityScenario<MyActivity>
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
 
     @Test
     fun useAppContext() {
 
-        scenario = launch(MyActivity::class.java)
+        scenario = launch(MainActivity::class.java)
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
 
         onView(withId(R.id.nav_host_fragment)).check(matches(isDisplayed()))
-
-        scenario.close()
     }
 }
