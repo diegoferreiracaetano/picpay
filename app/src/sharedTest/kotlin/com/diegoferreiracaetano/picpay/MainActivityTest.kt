dac0b3d5f1ec31573c41431959c5f0a4523ae6bf
diego ferreira caetano <di.cafe@hotmail.com>
2019-09-21 23:18:52 -0300

    ajuste teste mainactivity

--- app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/MainActivityTest.kt
+++ app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/MainActivityTest.kt
@@ -4,8 +4,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.runner.AndroidJUnit4
-import com.diegoferreiracaetano.contacts.R
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,7 +12,7 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     @Test
-    fun whenInitScreen() {
+    fun whenInitScreen_verifyIdNavHost() {
         ActivityScenario.launch(MainActivity::class.java)
 
         onView(withId(R.id.nav_host_fragment)).check(matches(isDisplayed()))
