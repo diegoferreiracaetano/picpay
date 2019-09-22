fe41afe38fb53e7caf1c107ab9595ff4d8bdd64d
diego ferreira caetano <di.cafe@hotmail.com>
2019-09-22 09:23:05 -0300

    ajuste teste mainactivity

--- app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/ui/MainActivityTest.kt
+++ app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/ui/MainActivityTest.kt
@@ -1,5 +1,6 @@
 package com.diegoferreiracaetano.picpay.ui
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -7,27 +8,27 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.diegoferreiracaetano.picpay.R
 import org.junit.After
-import org.junit.Rule
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @get:Rule
-    val rule = lazyActivityScenarioRule<MainActivity>(launchActivity = false)
+    private lateinit var scenario: ActivityScenario<MainActivity>
 
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
 
     @After
     fun after() {
-        rule.getScenario().close()
+        scenario.close()
     }
 
     @Test
     fun useAppContext() {
-
-        rule.launch()
-
         onView(withId(R.id.nav_host_fragment)).check(matches(isDisplayed()))
     }
 }
