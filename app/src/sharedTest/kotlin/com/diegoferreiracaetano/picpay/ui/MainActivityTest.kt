5ffff8ad311ef28c297de23c3bf3a8ec4a0658f4
diego ferreira caetano <di.cafe@hotmail.com>
2019-09-22 10:20:15 -0300

    ajuste teste mainactivity

--- app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/ui/MainActivityTest.kt
+++ app/src/sharedTest/kotlin/com/diegoferreiracaetano/picpay/ui/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.diegoferreiracaetano.picpay.ui
 
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -9,7 +10,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.diegoferreiracaetano.picpay.R
 import org.junit.After
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,11 +17,6 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     private lateinit var scenario: ActivityScenario<MainActivity>
-
-    @Before
-    fun setup() {
-        scenario = ActivityScenario.launch(MainActivity::class.java)
-    }
 
     @After
     fun after() {
@@ -31,7 +26,9 @@ class MainActivityTest {
     @Test
     fun useAppContext() {
 
-        scenario.moveToState(Lifecycle.State.CREATED)
+        scenario = launch(MainActivity::class.java)
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
 
         onView(withId(R.id.nav_host_fragment)).check(matches(isDisplayed()))
     }
